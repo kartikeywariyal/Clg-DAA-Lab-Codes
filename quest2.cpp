e0d7bc7c6b3705082618af3c88f8382e6b3dad1b
kartikeywariyal <151852657+kartikeywariyal@users.noreply.github.com>
2025-02-03 09:38:42 +0530

    Create quest2.cpp

---
+++ quest2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+void solve(){
+  int n;
+  cin>>n;
+  vector<int>a(n);
+  for(int &i:a)cin>>i;
+  int key;
+  cin>>key;
+  
+  int l=0,r=n-1;
+  int ans=0;
+  while(l<=r){
+    int mid=l+(r-l)/2;
+    if(a[mid]>=key){
+      ans=mid;
+      r=mid-1;
+    }
+    else l=mid+1;
+  }
+  if(a[ans]==key){
+    cout<<"Element found at index "<<ans<<endl;
+    return;
+  }
+  cout<<"ELement not found\n";
+}
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--)
+  solve();
+}
